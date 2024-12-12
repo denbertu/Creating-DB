f50468e34b57a1c16a651edee610d9692253f26b
Denis Klochikhin <denbertu@outlook.com>
2024-12-12 21:19:14 +0300

    Заданик 3.sql was changed

--- Заданик 3.sql
+++ Заданик 3.sql
@@ -23,10 +23,11 @@ GROUP BY album_id;
 
   SELECT DISTINCT collection_id
     FROM tracks_collections
-   WHERE track_id IN (SELECT track_id
-                        FROM tracks
-                       WHERE album_id = (SELECT album_id
-                                           FROM album_artist
-                                          WHERE artist_id = 3))
-
+   WHERE track_id IN 
+         (SELECT track_id
+            FROM tracks
+           WHERE album_id = 
+                 (SELECT album_id
+                    FROM album_artist
+                   WHERE artist_id = 3))
 
